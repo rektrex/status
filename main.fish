94f43674ec444af686b2fc7bea7fed27c6f7a277
rektrex <psaimanideep@gmail.com>
2019-09-22 11:26:56 +0530

    update script paths

--- main.fish
+++ main.fish
@@ -1,8 +1,8 @@
 #!/usr/bin/fish
 
-set battery (source ~/Desktop/LinuxProjects/status/battery.fish)
-set network (source ~/Desktop/LinuxProjects/status/network.fish)
-set time_date (source ~/Desktop/LinuxProjects/status/time_date.fish)
-set volume (source ~/Desktop/LinuxProjects/status/volume.fish)
+set battery (source ~/Projects/Linux/status/battery.fish)
+set network (source ~/Projects/Linux/status/network.fish)
+set time_date (source ~/Projects/Linux/status/time_date.fish)
+set volume (source ~/Projects/Linux/status/volume.fish)
 
 notify-send -u "Low" "a" (echo "$battery $network $time_date $volume")
