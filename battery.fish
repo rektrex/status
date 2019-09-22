9e874575375d355ec8fb929c098fe79cd01a38e1
rektrex <psaimanideep@gmail.com>
2019-09-22 22:46:35 +0530

    make battery value green during charging

--- battery.fish
+++ battery.fish
@@ -7,7 +7,7 @@ set val (echo $acpi | cut -d " " -f 4 | sed 's/[^0-9%]*//g')
 
 if test "$state" = "Discharging"
     echo "<b><span foreground='#dc322f'>$val</span></b>"
-else if test "$state" = "Unknown"
+else if test "$state" = "Charging"
     echo "<b><span foreground='#88c563'>$val</span></b>"
 else
     echo "<b>$val</b>"
