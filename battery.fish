755ce09d151620cb8ab52420d8aefa6a949236e3
rektrex <psaimanideep@gmail.com>
2019-09-24 23:57:22 +0530

    change red color

--- battery.fish
+++ battery.fish
@@ -6,7 +6,7 @@ set state (echo $acpi | cut -d " " -f 3 | sed 's/[^a-zA-Z]*//g')
 set val (echo $acpi | cut -d " " -f 4 | sed 's/[^0-9%]*//g')
 
 if test "$state" = "Discharging"
-    echo "<b><span foreground='#dc322f'> $val</span></b>"
+    echo "<b><span foreground='#e76d6d'> $val</span></b>"
 else if test "$state" = "Charging"
     echo "<b><span foreground='#88c563'> $val</span></b>"
 else
