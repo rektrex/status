b502257910b04168bb9894c8b1a574922e98981c
rektrex <23721797+rektrex@users.noreply.github.com>
2019-12-22 19:42:43 +0530

    update paths

--- main.fish
+++ main.fish
@@ -1,8 +1,8 @@
 #!/usr/bin/fish
 
-set battery (source ~/Projects/Linux/status/battery.fish)
-set network (source ~/Projects/Linux/status/network.fish)
-set time_date (source ~/Projects/Linux/status/time_date.fish)
-set volume (source ~/Projects/Linux/status/volume.fish)
+set battery (source ~/Projects/Fish/status/battery.fish)
+set network (source ~/Projects/Fish/status/network.fish)
+set time_date (source ~/Projects/Fish/status/time_date.fish)
+set volume (source ~/Projects/Fish/status/volume.fish)
 
 notify-send -u "Low" "a" (echo "$battery  $network  $time_date  $volume")
